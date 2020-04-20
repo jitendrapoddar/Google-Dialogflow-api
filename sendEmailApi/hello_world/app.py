152ac895cee35e23cac6dfb9dd381be30d55e0b3
jitendrapoddar <jitendrapoddar24@gmail.com>
2020-04-20 22:53:41 +0530

    new Commit

--- sendEmailApi/hello_world/app.py
+++ sendEmailApi/hello_world/app.py
@@ -1,42 +1,61 @@
+from flask import Flask, request, make_response
 import json
+import os
+from sendEmailApi.SendEmail.sendEmail import EmailSender
+from logger import logger
+from sendEmailApi.email_templates import template_reader
 
-# import requests
+
+app = Flask(__name__)
+
+# geting and sending response to dialogflow
+@app.route('/webhook', methods=['POST'])
+def webhook():
+
+    req = request.get_json(silent=True, force=True)
+
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    #print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+# processing the request from dialogflow
+def processRequest(req):
+    log = logger.Log()
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    sessionID=req.get('responseId')
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    result = req.get("queryResult")
+    user_says=result.get("queryText")
+    log.write_log(sessionID, "User Says: "+user_says)
+    parameters = result.get("parameters")
+    cust_name=parameters.get("cust_name")
+    #print(cust_name)
+    cust_contact = parameters.get("cust_contact")
+    cust_email=parameters.get("cust_email")
+    course_name= parameters.get("course_name")
+    intent = result.get("intent").get('displayName')
+    if (intent=='course_selection'):
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+        email_sender=EmailSender()
+        template= template_reader.TemplateReader()
+        email_message=template.read_course_template(course_name)
+        email_sender.send_email_to_student(cust_email,email_message)
+        email_file_support = open("../email_templates/support_team_Template.html", "r")
+        email_message_support = email_file_support.read()
+        email_sender.send_email_to_support(cust_name=cust_name,cust_contact=cust_contact,cust_email=cust_email,course_name=course_name,body=email_message_support)
+        fulfillmentText="We have sent the course syllabus and other relevant details to you via email. An email has been sent to the Support Team with your contact information, you'll be contacted soon. Do you have further queries?"
+        log.write_log(sessionID, "Bot Says: "+fulfillmentText)
+        return {
+            "fulfillmentText": fulfillmentText
+        }
+    else:
+        log.write_log(sessionID, "Bot Says: " + result.fulfillmentText)
